69646a7616aa095d9f53627cad38d49ed68bfffc
jrogercoker@gmail.com <jrogercoker@gmail.com>
2020-12-18 14:15:30 -0500

    chamged to updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -223,6 +223,7 @@ module.exports = {
               return allMarkdownRemark.edges.map((edge) => {
                 return Object.assign({}, edge.node.frontmatter, {
                   description: edge.node.excerpt,
+                  date: edge.node.updated
                   url: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   custom_elements: [{ "content:encoded": edge.node.html }],
