aefb45882e78a0cb35e3c5c9c6567b546a2ac5f0
jrogercoker@gmail.com <jrogercoker@gmail.com>
2021-08-11 07:31:55 -0400

    added GATSBY_ to fix gatsby cloud issue

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,12 +2,12 @@ import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 import Hit from './Hit'
 
-const appId = process.env.ALGOLIA_APP_ID;
-const searchKey = process.env.ALGOLIA_SEARCH_ONLY_API_KEY;
+const appId = process.env.GATSBY_ALGOLIA_APP_ID;
+const searchKey = process.env.GATSBY_ALGOLIA_SEARCH_ONLY_API_KEY;
 const searchClient = algoliasearch(appId, searchKey);
 
 const Search = () => (
-  <InstantSearch searchClient={searchClient} indexName={process.env.ALGOLIA_INDEX_NAME}>
+  <InstantSearch searchClient={searchClient} indexName={process.env.GATSBY_ALGOLIA_INDEX_NAME}>
     <SearchBox />
     <Hits hitComponent={Hit}/>
   </InstantSearch>
