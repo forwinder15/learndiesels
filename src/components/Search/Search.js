3a2a89145cbc7f9dae4d097b1e1e155a2640f4d6
jrogercoker@gmail.com <jrogercoker@gmail.com>
2021-08-10 22:54:54 -0400

    change search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,7 +2,8 @@ import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 import Hit from './Hit'
 
-const searchClient = algoliasearch('ZPBUD4MZU8', '3b1f3afb40ea9208dadd0891343620b3');
+
+const searchClient = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_SEARCH_ONLY_API_KEY);
 
 const Search = () => (
   <InstantSearch searchClient={searchClient} indexName="CONTENT">
